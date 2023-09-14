0fdb47fc67277d84ac8b914ba86e1fa8f75637c5
ArtisBaseCode <artis@basecode.al>
2023-09-14 16:36:47 +0200

    Some simple logic implemented

--- src/app/components/movie-box/movie-box.component.ts
+++ src/app/components/movie-box/movie-box.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movie-box',
   templateUrl: './movie-box.component.html',
-  styleUrls: ['./movie-box.component.css']
+  styleUrls: ['./movie-box.component.css'],
 })
 export class MovieBoxComponent {
-
+  @Input() title: string | undefined;
+  @Input() categories: string[] | undefined;
+  @Input() imgUrl: string | undefined;
 }
